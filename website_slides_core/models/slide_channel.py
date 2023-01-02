da1e909764724847ff68c536a9b0bfe9a578a4e0
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-01-02 14:20:22 +0200

    Modified function

--- website_slides_core/models/slide_channel.py
+++ website_slides_core/models/slide_channel.py
@@ -16,31 +16,32 @@ class SlideChannel(models.Model):
         response = super(SlideChannel, self)._action_add_members(
             target_partners, **member_values
         )
-        values = {
-            "name": response.partner_id.name,
-            "partner_id": response.partner_id.id,
-            "login": response.partner_id.email,
-        }
-        get_param = self.env["ir.config_parameter"].sudo().get_param
-        channel_create_user_param = get_param("channel.create_user")
-        if channel_create_user_param == "True":
-            if response.partner_id:
-                values = {
-                    "name": response.partner_id.name,
-                    "partner_id": response.partner_id.id,
-                    "login": response.partner_id.email,
-                }
-                partner_user = (
-                    response.partner_id.user_ids
-                    and response.partner_id.user_ids[0]
-                    or False
-                )
-                if not partner_user:
-                    new_user = self.env["res.users"]._signup_create_user(values)
-                    if new_user:
-                        new_user.with_context(create_user=True).action_reset_password()
-        else:
-            return response
+        for r in response:
+            values = {
+                "name": r.partner_id.name,
+                "partner_id": r.partner_id.id,
+                "login": r.partner_id.email,
+            }
+            get_param = self.env["ir.config_parameter"].sudo().get_param
+            channel_create_user_param = get_param("channel.create_user")
+            if channel_create_user_param == "True":
+                if r.partner_id:
+                    values = {
+                        "name": r.partner_id.name,
+                        "partner_id": r.partner_id.id,
+                        "login": r.partner_id.email,
+                    }
+                    partner_user = (
+                        r.partner_id.user_ids
+                        and r.partner_id.user_ids[0]
+                        or False
+                    )
+                    if not partner_user:
+                        new_user = self.env["res.users"]._signup_create_user(values)
+                        if new_user:
+                            new_user.with_context(create_user=True).action_reset_password()
+            else:
+                return r
 
     @api.onchange("product_id")
     def _onchange_product_id(self):
