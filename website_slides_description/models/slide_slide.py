c2b657bfd6496b88a9795076c763421f3153cc96
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2023-07-04 16:57:17 +0300

    Created new module

---
+++ website_slides_description/models/slide_slide.py
@@ -0,0 +1,9 @@
+from odoo import fields
+from odoo import models
+from odoo.tools.translate import html_translate
+
+
+class SlideSlide(models.Model):
+    _inherit = "slide.slide"
+
+    website_description = fields.Html('Website Description', sanitize_attributes=False, translate=html_translate, sanitize_form=False)
