fe28aeeca17f07e6d3b0a05c0bad5b2944007aa4
Timo Kekäläinen <timo.kekalainen@hotmail.com>
2023-09-15 06:21:36 +0000

    [ADD] This commit adds a new module: website_slides_sort_content_by_category

---
+++ website_slides_sort_content_by_category/models/slide_slide.py
@@ -0,0 +1,25 @@
+
+from odoo import api, fields, models
+
+
+class SlideSlide(models.Model):
+
+    _inherit = 'slide.slide'
+
+    category_sort_id = fields.Many2one('slide.slide', string="Choose a section", store=True)
+
+    @api.depends('channel_id.slide_ids.is_category', 'channel_id.slide_ids.sequence', 'category_sort_id')
+    def _compute_category_id(self):
+        res = super()._compute_category_id()
+        # Force a chosen category
+        for slide in self:
+            if slide.category_sort_id:
+                slide.category_id = slide.category_sort_id
+        return res
+
+    @api.model
+    def create(self, vals):
+        category_sort_id = vals.get('category_sort_id')
+        category_sort_id = self.env['slide.slide'].browse(category_sort_id)
+        vals['sequence'] = category_sort_id.sequence + 1
+        return super().create(vals)
