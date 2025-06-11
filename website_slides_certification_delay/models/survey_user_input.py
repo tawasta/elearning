94ca51d962f9e8135b837591ef0800f3990cc3fa
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-06-11 10:18:13 +0300

    [IMP] - created new module

--- website_slides_certification_delay/models/survey_user_input.py
+++ website_slides_certification_delay/models/survey_user_input.py
@@ -1,24 +1,31 @@
 from odoo import models, fields
 from datetime import datetime, timedelta
 
+
 class SurveyUserInput(models.Model):
-    _inherit = 'survey.user_input'
+    _inherit = "survey.user_input"
 
     def write(self, vals):
-        was_done_failures = self.filtered(lambda r: r.state == 'done' and not r.scoring_success and r.slide_partner_id)
+        was_done_failures = self.filtered(
+            lambda r: r.state == "done" and not r.scoring_success and r.slide_partner_id
+        )
 
         res = super().write(vals)
 
-        now_failed = self.filtered(lambda r: r.state == 'done' and not r.scoring_success and r.slide_partner_id)
+        now_failed = self.filtered(
+            lambda r: r.state == "done" and not r.scoring_success and r.slide_partner_id
+        )
         new_failures = now_failed - was_done_failures
 
         for rec in new_failures:
             retry_delay = rec.slide_partner_id.channel_id.retry_delay_hours or 0
             retry_after = datetime.utcnow() + timedelta(hours=retry_delay)
 
-            rec.slide_partner_id.write({
-                'last_failed_attempt': fields.Datetime.now(),
-                'next_retry_possible_at': retry_after,
-            })
+            rec.slide_partner_id.write(
+                {
+                    "last_failed_attempt": fields.Datetime.now(),
+                    "next_retry_possible_at": retry_after,
+                }
+            )
 
         return res
