118fbd3c65feb0daebe32ed0a2dfa028cb60d80c
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-06-13 10:40:55 +0300

    [IMP] - modified function and added timezone

--- website_slides_certification_delay/models/survey_user_input.py
+++ website_slides_certification_delay/models/survey_user_input.py
@@ -5,6 +5,7 @@ import pytz
 
 _logger = logging.getLogger(__name__)
 
+
 class SurveyUserInput(models.Model):
     _inherit = "survey.user_input"
 
@@ -20,18 +21,22 @@ class SurveyUserInput(models.Model):
         )
         new_failures = now_failed - was_done_failures
 
-        fin_tz = pytz.timezone('Europe/Helsinki')
+        fin_tz = pytz.timezone("Europe/Helsinki")
 
         for rec in new_failures:
             retry_delay = rec.slide_partner_id.channel_id.retry_delay_hours or 0
 
             now_utc = fields.Datetime.now()
-            now_local = now_utc.replace(tzinfo=pytz.UTC).astimezone(fin_tz).replace(tzinfo=None)
+            now_local = (
+                now_utc.replace(tzinfo=pytz.UTC).astimezone(fin_tz).replace(tzinfo=None)
+            )
             retry_after_local = now_local + timedelta(hours=retry_delay)
 
-            rec.slide_partner_id.write({
-                "last_failed_attempt": now_local,
-                "next_retry_possible_at": retry_after_local,
-            })
+            rec.slide_partner_id.write(
+                {
+                    "last_failed_attempt": now_local,
+                    "next_retry_possible_at": retry_after_local,
+                }
+            )
 
         return res
