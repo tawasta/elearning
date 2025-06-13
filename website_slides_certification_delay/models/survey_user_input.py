6572d586b57b8e35bb8cc576a471aa8bd0bdc7b1
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-06-13 10:39:16 +0300

    [IMP] - modified function and added timezone

--- website_slides_certification_delay/models/survey_user_input.py
+++ website_slides_certification_delay/models/survey_user_input.py
@@ -1,6 +1,9 @@
 from odoo import models, fields
-from datetime import datetime, timedelta
+from datetime import timedelta
+import logging
+import pytz
 
+_logger = logging.getLogger(__name__)
 
 class SurveyUserInput(models.Model):
     _inherit = "survey.user_input"
@@ -17,15 +20,18 @@ class SurveyUserInput(models.Model):
         )
         new_failures = now_failed - was_done_failures
 
+        fin_tz = pytz.timezone('Europe/Helsinki')
+
         for rec in new_failures:
             retry_delay = rec.slide_partner_id.channel_id.retry_delay_hours or 0
-            retry_after = datetime.utcnow() + timedelta(hours=retry_delay)
 
-            rec.slide_partner_id.write(
-                {
-                    "last_failed_attempt": fields.Datetime.now(),
-                    "next_retry_possible_at": retry_after,
-                }
-            )
+            now_utc = fields.Datetime.now()
+            now_local = now_utc.replace(tzinfo=pytz.UTC).astimezone(fin_tz).replace(tzinfo=None)
+            retry_after_local = now_local + timedelta(hours=retry_delay)
+
+            rec.slide_partner_id.write({
+                "last_failed_attempt": now_local,
+                "next_retry_possible_at": retry_after_local,
+            })
 
         return res
