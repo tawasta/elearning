2cda00c402f76173d831b9775fa6b925f3713780
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-08-11 15:46:07 +0300

    [IMP] - modified logic and timezone

--- website_slides_certification_delay/models/slide_partner.py
+++ website_slides_certification_delay/models/slide_partner.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class SlideSlidePartner(models.Model):
@@ -6,3 +6,18 @@ class SlideSlidePartner(models.Model):
 
     last_failed_attempt = fields.Datetime("Last Failed Attempt", readonly=True)
     next_retry_possible_at = fields.Datetime("Next Retry Allowed At")
+
+    next_retry_local = fields.Datetime(
+        string='Next Retry (Local Time)',
+        compute='_compute_next_retry_local',
+        store=False,
+    )
+
+    @api.depends('next_retry_possible_at')
+    def _compute_next_retry_local(self):
+        for rec in self:
+            if rec.next_retry_possible_at:
+                dt_aware = fields.Datetime.context_timestamp(self.env.user, rec.next_retry_possible_at)
+                rec.next_retry_local = dt_aware.replace(tzinfo=None) if dt_aware else False
+            else:
+                rec.next_retry_local = False
