10ace645f8711c64d3170a39fa943141f417ebca
Valtteri Lattu <valtteri.lattu@tawasta.fi>
2025-08-11 15:48:34 +0300

    [IMP] - modified logic and timezone

--- website_slides_certification_delay/models/slide_partner.py
+++ website_slides_certification_delay/models/slide_partner.py
@@ -8,16 +8,20 @@ class SlideSlidePartner(models.Model):
     next_retry_possible_at = fields.Datetime("Next Retry Allowed At")
 
     next_retry_local = fields.Datetime(
-        string='Next Retry (Local Time)',
-        compute='_compute_next_retry_local',
+        string="Next Retry (Local Time)",
+        compute="_compute_next_retry_local",
         store=False,
     )
 
-    @api.depends('next_retry_possible_at')
+    @api.depends("next_retry_possible_at")
     def _compute_next_retry_local(self):
         for rec in self:
             if rec.next_retry_possible_at:
-                dt_aware = fields.Datetime.context_timestamp(self.env.user, rec.next_retry_possible_at)
-                rec.next_retry_local = dt_aware.replace(tzinfo=None) if dt_aware else False
+                dt_aware = fields.Datetime.context_timestamp(
+                    self.env.user, rec.next_retry_possible_at
+                )
+                rec.next_retry_local = (
+                    dt_aware.replace(tzinfo=None) if dt_aware else False
+                )
             else:
                 rec.next_retry_local = False
